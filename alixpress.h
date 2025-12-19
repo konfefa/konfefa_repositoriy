a96fef34b03ba7f7270c10cb7d6720c9f21f9b0a
konfefa <nn.anastasiya.nn@gmail.com>
2025-12-19 17:14:02 +0000

    сделала 5 лабу

--- alixpress.h
+++ alixpress.h
@@ -21,18 +21,17 @@ void free_arr(Arr *a);
 void add_item(Arr *a, Item i);
 void remove_item(Arr *a, int idx);
 Item* get_item(Arr *a, int idx);
-void save(Arr *a, const char *filename);  // изменено
-void load(Arr *a, const char *filename);  // изменено
+void save(Arr *a, const char *filename); 
+void load(Arr *a, const char *filename);
 void show_all(Arr *a);
 void find_name(Arr *a, char *name);
 void find_price(Arr *a, float min, float max);
 void find_cat(Arr *a, char *cat);
-void add_new(Arr *a, const char *filename);  // изменено
-void delete_item(Arr *a, const char *filename);  // изменено
-void edit_item(Arr *a, const char *filename);  // изменено
+void add_new(Arr *a, const char *filename);  
+void delete_item(Arr *a, const char *filename); 
+void edit_item(Arr *a, const char *filename);  
 void menu();
 
-// Вспомогательные функции для валидации
 int read_int(const char* prompt, int min_val, int max_val);
 float read_float(const char* prompt, float min_val);
 void read_string(const char* prompt, char* buffer, int max_len);
